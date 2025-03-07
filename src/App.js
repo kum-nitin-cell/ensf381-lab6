41d62d14b0ccadec43ca90d401f665025ecb28c4
kum-nitin-cell <nitin.kumar2@ucalgary.ca>
2025-03-07 13:29:36 -0700

    final changes made

--- src/App.js
+++ src/App.js
@@ -20,9 +20,13 @@ function App() {
       <p>"React Components"</p>
       <p>{currentyear}</p>
       <p>{message()}</p>
+
+
       <Home title="Home Page" description="Welcome to our website." />
       <About title="About Us" description="We are passionate about delivering quality experiences." />
       <Contact title="Contact Us" description="Feel free to reach out to us via email or phone." />
+
+
       <EngineeringTopics/>
 
     </div>
