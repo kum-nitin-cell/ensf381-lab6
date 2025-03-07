1297a33e671d95d3f641d8af521e309b3e22a6c6
kum-nitin-cell <nitin.kumar2@ucalgary.ca>
2025-03-07 13:08:38 -0700

    exercise 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import EngineeringTopics from './EngineeringTopics';
 
 function App() {
   const currentyear = new Date().getFullYear();
@@ -22,6 +23,8 @@ function App() {
       <Home title="Home Page" description="Welcome to our website." />
       <About title="About Us" description="We are passionate about delivering quality experiences." />
       <Contact title="Contact Us" description="Feel free to reach out to us via email or phone." />
+
+      
     </div>
   );
 }
