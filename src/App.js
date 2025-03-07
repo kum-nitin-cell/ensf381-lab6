858142b0bf1e01a88d35857bdb144b6797a581ac
kum-nitin-cell <nitin.kumar2@ucalgary.ca>
2025-03-07 12:11:45 -0700

    exercise 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const currentyear = new Date().getFullYear();
+  const isLoggedIn = true;
+
+  function message(){
+    if(isLoggedIn)
+      return "Welcome back!"
+    return "please log in"
+  }
+
+  return ( 
+    <div>
+      <h1>"ENSF-381: Full Stack Web Development"</h1>
+      <p>"React Components"</p>
+      <p>{currentyear}</p>
+      <p>{message()}</p>
+      <Home />
+      <About />
+      <Contact />
     </div>
   );
 }
 
 export default App;
+
+
