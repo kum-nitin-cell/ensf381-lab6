547ee299698743127f1d1df4c43c85e9875bc2b9
kum-nitin-cell <nitin.kumar2@ucalgary.ca>
2025-03-07 13:18:20 -0700

    exercise 3 final

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ function App() {
       <Home title="Home Page" description="Welcome to our website." />
       <About title="About Us" description="We are passionate about delivering quality experiences." />
       <Contact title="Contact Us" description="Feel free to reach out to us via email or phone." />
+      <EngineeringTopics/>
 
-      
     </div>
   );
 }
